4d01dd205140a1b5df86665d245a07406c90a1fc
arunimajayan23 <132655034+arunimajayan23@users.noreply.github.com>
2023-05-05 19:28:04 +0530

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,37 +1,56 @@
-def add(x, y):
-    return x + y
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self,x, y):
+        try:
+            return x / y
+        except ZeroDivisionError:
+            print("Error: division by zero is undefinable")
+            return None
+
+def main():
+    calculator = Calculator()
+    while True:
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    while True:
+        print("Select operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+        choice = input("Enter choice (1/2/3/4/5): ")
+
+        if choice == '1':
+            result = calculator.add(num1, num2)
+            print(num1, "+", num2, "=", result)
+        elif choice == '2':
+            result = calculator.subtract(num1, num2)
+            print(num1, "-", num2, "=", result)
+        elif choice == '3':
+            result = calculator.multiply(num1, num2)
+            print(num1, "*", num2, "=", result)
+        elif choice == '4':
+            result = calculator.divide(num1, num2)
+            if result is not None:
+                print(num1, "/", num2, "=", result)
+        elif choice =='5':
+            break
+        else:
+            print("Invalid input")
 
 
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    return x / y
-
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-
-choice = input("Enter choice (1/2/3/4): ")
-
-if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
-elif choice == '2':
-    print(num1, "-", num2, "=", subtract(num1, num2))
-elif choice == '3':
-    print(num1, "*", num2, "=", multiply(num1, num2))
-elif choice == '4':
-    print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Invalid input")
+if __name__ == '__main__':
+    main()
