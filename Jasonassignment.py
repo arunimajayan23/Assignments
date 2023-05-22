8daffee1f2155cbba68d33c56126313b1b60e255
arunimajayan23 <132655034+arunimajayan23@users.noreply.github.com>
2023-05-22 15:11:30 +0530

    Add files via upload

--- Jasonassignment.py
+++ Jasonassignment.py
@@ -2,17 +2,17 @@ import json
 
 with open(r"C:\Users\arunima.jayan\Downloads\sample_data.json", "r") as file:
     content = json.load(file)
-
-outer_dict = {}
+print(type(content))
+outer_list = []
 parameter_content = content["parametersList"]
 for one in parameter_content:
     inner_dict = {}
-    key = one["parameterName"]
+    inner_dict["parameterName"] = one["parameterName"]
     inner_dict["min"] = one["min"]
     inner_dict["max"] = one["max"]
     inner_dict["avg"] = one["avg"]
-    outer_dict[key] = inner_dict
+    outer_list.append(inner_dict)
 
-print(outer_dict)
-with open(r"C:\Users\arunima.jayan\Downloads\sample_data_out.json", "w") as file:
-    json.dump(outer_dict, file)
+print(outer_list)
+with open(r"C:\Users\arunima.jayan\Downloads\sample_data_out1.json", "w") as file:
+    json.dump(outer_list, file)
